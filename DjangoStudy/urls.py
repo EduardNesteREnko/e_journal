823666eb282f3e0d3c7f1c24a38d344175e4cd36
Edward <nasviratusforchen@gmail.com>
2026-03-11 11:52:30 +0200

    Users

--- DjangoStudy/urls.py
+++ DjangoStudy/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from xml.etree.ElementInclude import include
+from django.urls import path, include
+#rom xml.etree.ElementInclude import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('common.urls')),
+    path('', include('common.urls')),
     path('student/', include('student.urls')),
     path('teacher/', include('teacher.urls')),
     path('parents/', include('parents.urls')),
--- common/views.py
+++ common/views.py
@@ -1,17 +1,47 @@
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
     if request.method == 'POST':
-        return 'ok'
+        user_login = request.POST.get('login')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, username=user_login, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/user/' + str(user.id) + '/')
+        else:
+            return render(request, 'login.html', context={'error': 'Invalid login or password'})
+
     else:
-        return render('login.html')
+        return render(request,'login.html')
 
 def register(request):
-    return 'ok'
+    if request.method == 'POST':
+        user_login = request.POST.get('login')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        user = User.objects.create_user(username=user_login,
+    email=email,
+    password=password,
+    first_name=first_name,
+    last_name=last_name)
+        user.save()
+        return redirect('login')
+    else:
+        return render(request,'register.html')
 
 def logout(request):
+    logout(request)
     return redirect('login')
 
+@login_required(login_url='login')
 def user_view(request, user_id):
-    return f'ok {user_id}'
+    user = User.objects.get(id=user_id)
+    user_groups = user.groups.all()[0]
+    return render(request, 'user_page.html', context={'user': user, 'group_name': user_groups.name})
